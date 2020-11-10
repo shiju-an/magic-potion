317360b5e5665f39b21fdab477c09865d58e0a80
Joanna Wang <shiju.an362@gmail.com>
2020-11-09 20:16:24 -0800

    connect basic server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const path = require('path');
 const DIST_DIR = path.resolve(__dirname, '..', 'client', 'dist');
+const port = 3000;
 
 app.use(express.static(DIST_DIR));
+
+app.use((req, res, next) => {
+  res.redirect("/")
+});
 
 app.listen(port, () => console.log(`do you come in, port ${port}`));
