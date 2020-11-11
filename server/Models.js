ba0d98f7723a460177f5ca80f808fbb154e5db37
Joanna Wang <shiju.an362@gmail.com>
2020-11-10 21:35:55 -0800

    flesh out validation to save orders

--- server/Models.js
+++ server/Models.js
@@ -13,7 +13,14 @@ const getOrder = (id, callback) => {
 };
 
 const saveOrder = (newOrder, callback) => {
-  console.log(newOrder);
+  //check if user exists in user table
+    //if not, insert user and transaction data
+  //else user exists in table
+    //if user exists, check if transaction quantities < 3 and if transaction quantities + current quantities < 3
+      //if so, insert transaction with existing user id 
+    //if not, don't do anything
+
+  
 };
 
 const updateOrder = (id, callback) => {
