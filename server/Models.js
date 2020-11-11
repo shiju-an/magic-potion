ba35204306ceef7360ad397c5b4527c220984480
Joanna Wang <shiju.an362@gmail.com>
2020-11-11 00:07:13 -0800

    callbacks on callbacks on callbacks after promise attempt

--- server/Models.js
+++ server/Models.js
@@ -1,5 +1,7 @@
 const db = require('../db');
 
+//need to destructure + add as param for query (all)
+
 const getOrder = (id, callback) => {
   db.query(
     `SELECT * FROM transactions INNER JOIN users ON transactions.userId = users.id WHERE transactions.id = ${id}`, (err, data) => {
@@ -18,6 +20,61 @@ const saveOrder = (newOrder, callback) => {
     //if user exists, check if transaction quantities < 3 and if transaction quantities + current quantities < 3
       //if so, insert transaction with existing user id 
     //if not, don't do anything
+  //promisify?
+
+  //test insert
+  let { firstName, lastName, quantity, total, expMM, expYY } = newOrder;
+  let expDate = `${expMM}/${expYY}`
+
+  let checkUserSql = `SELECT id FROM users WHERE first_name = ? AND last_name = ? AND exp_date = ? LIMIT 1`;
+
+  let insertOrderSql = `INSERT INTO transactions (quantity, total, fulfilled, order_date, userId) VALUES (?, ?, false, curdate(), ?)`;
+
+  let insertUserSql = `INSERT INTO users (first_name, last_name, exp_date) VALUES (?, ?, ?)`;
+
+  let selectId = 'SELECT id FROM users ORDER BY id DESC LIMIT 1';
+
+  db.query(checkUserSql, [firstName, lastName, expDate], (err, res) => {
+    if (err) {
+      console.log(err);
+    } else if (res.length === 0) {
+      console.log('before insert user');
+      db.query(insertUserSql, [firstName, lastName, expDate], (err) => {
+        if(err) {
+          console.log(err, 'error');
+        } else {
+          console.log('before select new id')
+          db.query(selectId, (err, id) => {
+            if (err) {
+              console.log('error');
+            } else {
+              console.log('before insert order');
+              db.query(insertOrderSql, [quantity, total, id[0].id], (err) => {
+                if(err){
+                  console.log(err, 'final error');
+                } else {
+                  console.log('how did this work');
+                } 
+              });
+            }
+          });
+        };
+      });
+    } else {
+      console.log('only add transaction if whatever order isnt greater than 3 by selected user');
+    }
+  })
+  
+    // .then(res => {
+    //   if (res > 0) {
+    //     console.log('hit');
+    //   } else {
+    //     console.log('test');
+    //   };
+    // })
+    // .catch(err => {
+    //   console.log(err, 'error promises lol');
+    // })
 };
 
 const updateOrder = (id, callback) => {
