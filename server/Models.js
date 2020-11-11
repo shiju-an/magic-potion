b026f1ff290938ae03f306e2fa1b1da09664f0d4
Joanna Wang <shiju.an362@gmail.com>
2020-11-10 20:52:18 -0800

    finish base get query

--- server/Models.js
+++ server/Models.js
@@ -1,7 +1,15 @@
 const db = require('../db');
 
 const getOrder = (id, callback) => {
-  console.log(id);
+  db.query(
+    `SELECT * FROM transactions INNER JOIN users ON transactions.userId = users.id WHERE transactions.id = ${id}`, (err, data) => {
+      if (err) {
+        console.log('error at db model thing', err);
+        callback(err);
+      } else {
+        callback(null, data);
+      };
+    });
 };
 
 const saveOrder = (newOrder, callback) => {
