d60e288b45def4a9ec4e8f04313fccc1f0c3e8b2
Joanna Wang <shiju.an362@gmail.com>
2020-11-12 09:56:04 -0800

    finish basic form styling

--- client/src/components/ContactForm.jsx
+++ client/src/components/ContactForm.jsx
@@ -5,61 +5,81 @@ const ContactForm = ({
   validateName, validatePhoneNumber
 }) => (
   <div className="formContainer">
-    <div className="titleContainer">
-      <h2>USER INFO</h2>>
-    </div>
+    <h2 id="title">USER INFO</h2>
+    <div className="titleContainer"></div>
+
+  <div className="formBox">
     <form>
-      <label>
-        First Name: 
-      </label>
-      <input 
-        id='firstNameInput'
-        name='firstName'
-        type='text' 
-        placeholder='First Name'
-        value={firstName}
-        onChange={onChange}
-        onBlur={validateName}
-      />
+      <div className="formField">
+        <label className="formDesc">
+          First Name: 
+        </label>
+        <input 
+          className="formInput"
+          id='firstNameInput'
+          name='firstName'
+          type='text' 
+          // placeholder='First Name'
+          value={firstName}
+          onChange={onChange}
+          onBlur={validateName}
+        />
+      </div>
 
-      <label>
-        Last Name: 
-      </label>
-      <input 
-        id='lastNameInput'
-        name='lastName'
-        type='text' 
-        placeholder='Last Name'
-        value={lastName}
-        onChange={onChange}
-        onBlur={validateName}
-      />
+      <div className="formField">
+        <label className="formDesc">
+          Last Name: 
+        </label>
+        <input 
+          className="formInput"
+          id='lastNameInput'
+          name='lastName'
+          type='text' 
+          // placeholder='Last Name'
+          value={lastName}
+          onChange={onChange}
+          onBlur={validateName}
+        />
+      </div>
 
-      <label>
-        Email:
-      </label>
-      <input 
-        id='emailInput'
-        name='email'
-        type='email' 
-        placeholder='Email Address'
-        value={email}
-        onChange={onChange}
-      />         
+      <div className="formField">
+        <label className="formDesc">
+          Email:
+        </label>
+        <input 
+          className="formInput"
+          id='emailInput'
+          name='email'
+          type='email' 
+          // placeholder='Email Address'
+          value={email}
+          onChange={onChange}
+        />         
+      </div>
 
-      <label>
-        Phone Number:
-      </label>
-      <input 
-        id='phoneInput'
-        name='phone'
-        type='text' 
-        placeholder='Phone Number'
-        value={phone}
-        onChange={onChange}
-        onBlur={validatePhoneNumber}
-      />      
+      <div className="formField">
+        <label className="formDesc">
+          Phone Number:
+        </label>
+        <input 
+          className="formInput"
+          id='phoneInput'
+          name='phone'
+          type='text' 
+          // placeholder='Phone Number'
+          value={phone}
+          onChange={onChange}
+          onBlur={validatePhoneNumber}
+        />      
+      </div>
     </form>
+  </div>
+
+  <div className="bottomArrows">
+    <button 
+      id="next">Next</button>
+  </div>
+  
   </div>
 );
 
