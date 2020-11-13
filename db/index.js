f99226561247a3b08eb0a1c848ace0a1e7c4f76b
Joanna Wang <shiju.an362@gmail.com>
2020-11-12 16:29:06 -0800

    attempt to handle disconnect in production

--- db/index.js
+++ db/index.js
@@ -3,12 +3,26 @@ const mysql = require('mysql');
 
 const connection = mysql.createConnection('mysql://b75352c28ce8dc:4c06cf8c@us-cdbr-east-02.cleardb.com/heroku_e98030fc61e2c0f?reconnect=true');
 
-connection.connect((err) => {
-  if(err) {
-    console.log('db connection error', err)
-  } else {
-    console.log('do you come in potion database')
-  }
-});
+function handleDisconnect() {
+  connection.connect((err) => {
+    if(err) {
+      console.log('db connection error', err)
+      setTimeOut(handleDisconnect, 2000);
+    } else {
+      console.log('do you come in potion database')
+    }
+  });
+
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();          
+    } else {          
+      throw err;    
+    }
+  });
+}
+
+handleDisconnect();
 
 module.exports = connection;
