966be1e5d999174f2f174b7cc003ccdd4bce6282
Joanna Wang <shiju.an362@gmail.com>
2020-11-12 22:33:09 -0800

    update readme

--- db/index.js
+++ db/index.js
@@ -1,23 +1,6 @@
 const mysql = require('mysql');
 
-var pool = mysql.createPool({
-  connectionLimit: 5,
-  host: 'us-cdbr-east-02.cleardb.com',
-  user: 'b75352c28ce8dc',
-  password: '4c06cf8c',
-  database: 'heroku_e98030fc61e2c0f'
-})
-
-pool.query('SELECT 1 + 1 AS solution', (err, res) => {
-  if (err) {
-    console.log(err, 'error connecting');
-  }
-  console.log('potion peddler');
-});
-  
-module.exports = pool;
-
-//TO BE USED LOCALLY: SEE BELOW, AND COMMENT OUT LINES ABOVE
+//TO BE USED LOCALLY: SEE BELOW
 //create config file with below filled to local enviornment
 // module.exports = {
 //   user: '',
