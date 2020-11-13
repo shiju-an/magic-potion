8b33ec6099c9390f515bc1521f4489ad5e1c8c8e
Joanna Wang <shiju.an362@gmail.com>
2020-11-12 16:44:02 -0800

    edit db connection

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 // const config = require('./config.js');
 
-const connection;
+var connection;
 
 function handleDisconnect() {
   connection = mysql.createConnection('mysql://b75352c28ce8dc:4c06cf8c@us-cdbr-east-02.cleardb.com/heroku_e98030fc61e2c0f?reconnect=true')
