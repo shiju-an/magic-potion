c9763107d49a434775b7a2f6b21e441ec7dae84e
Joanna Wang <shiju.an362@gmail.com>
2020-11-12 16:40:47 -0800

    try to handle connection lost to db for heroku

--- db/index.js
+++ db/index.js
@@ -1,16 +1,17 @@
 const mysql = require('mysql');
 // const config = require('./config.js');
 
-const connection = mysql.createConnection('mysql://b75352c28ce8dc:4c06cf8c@us-cdbr-east-02.cleardb.com/heroku_e98030fc61e2c0f?reconnect=true');
+const connection;
 
 function handleDisconnect() {
-  connection.connect((err) => {
+  connection = = mysql.createConnection('mysql://b75352c28ce8dc:4c06cf8c@us-cdbr-east-02.cleardb.com/heroku_e98030fc61e2c0f?reconnect=true')
+  
+  connection.connect(function(err) {
     if(err) {
       console.log('db connection error', err)
       setTimeout(handleDisconnect, 2000);
-    } else {
-      console.log('do you come in potion database')
     }
+    console.log('potion peddler')
   });
 
   connection.on('error', function(err) {
